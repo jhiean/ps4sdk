725385cd450fbed23ce6c57454d43eb4d1aac303
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2017-11-03 16:54:05 +0000

    cxx attempt

--- include/stdlib.h
+++ include/stdlib.h
@@ -148,6 +148,16 @@ void	 _Exit(int) __dead2;
 #endif /* __ISO_C_VISIBLE >= 1999 */
 
 /*
+ * If we're in a mode greater than C99, expose C11 functions.
+ */
+#if __ISO_C_VISIBLE >= 2011 || __cplusplus >= 201103L
+void *	aligned_alloc(size_t, size_t) __malloc_like;
+_Noreturn void
+	quick_exit(int);
+int	at_quick_exit(void (*)(void));
+#endif /* __ISO_C_VISIBLE >= 2011 */
+
+/*
  * Extensions made by POSIX relative to C.  We don't know yet which edition
  * of POSIX made these extensions, so assume they've always been there until
  * research can be done.
