638d3e492d6ef77029c96cecebcf155e8610336e
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-01-01 16:12:27 +0000

    fix build

--- base/kernel_dlsym/standard/source/kernel_symbol_lookup.c
+++ base/kernel_dlsym/standard/source/kernel_symbol_lookup.c
@@ -11,6 +11,23 @@ sym_t table405[] = {
 	{ NULL, 0 },
 };
 
+// taken from glibc
+int strcmp(const char *p1, const char *p2)
+{
+  const unsigned char *s1 = (const unsigned char *) p1;
+  const unsigned char *s2 = (const unsigned char *) p2;
+  unsigned char c1, c2;
+  do
+    {
+      c1 = (unsigned char) *s1++;
+      c2 = (unsigned char) *s2++;
+      if (c1 == '\0')
+        return c1 - c2;
+    }
+  while (c1 == c2);
+  return c1 - c2;
+}
+
 int static_lookup(const char *name, void **value, int swVer)
 {
 	void* kernel_base = ps4KernelSeekElfAddress();
