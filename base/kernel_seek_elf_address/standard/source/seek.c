674560e5a20cc8dc2b1c9fbc4d54de97a9109499
Mahmood(Thunder07) <m.a.hot.m+git@gmail.com>
2018-01-01 16:12:27 +0000

    fix build

--- base/kernel_seek_elf_address/standard/source/seek.c
+++ base/kernel_seek_elf_address/standard/source/seek.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdint.h>
 
 enum{ PS4_KERNEL_ELF_RANGE_SIZE = 0x02000000 };
 enum{ PS4_KERNEL_ELF_PAGE_SIZE = 16 * 1024 }; // no dependencies
@@ -28,7 +29,15 @@ void *ps4KernelSeekElfAddress()
 	unsigned char elfMagic[] = {0x7f, 'E', 'L', 'F', 0x02, 0x01, 0x01, 0x09, 0x00};
 	const size_t magicSize = sizeof(elfMagic) / sizeof(*elfMagic);
 
-	char *startaddr = findClosestPageStart(__readmsr(0xC0000082));
+	uint64_t rip;
+	{
+		unsigned long __edx;
+		unsigned long __eax;
+		__asm__ ("rdmsr" : "=d"(__edx), "=a"(__eax) : "c"(0xC0000082));
+		rip = (((uint64_t)__edx) << 32) | (uint64_t)__eax;
+	}
+
+	char *startaddr = findClosestPageStart(rip);
 	for(char *m = startaddr;
 		m > startaddr - PS4_KERNEL_ELF_RANGE_SIZE;
 		m -= PS4_KERNEL_ELF_PAGE_SIZE)
--- include/ps4/kernel/symbol.h
+++ include/ps4/kernel/symbol.h
@@ -1,4 +1,3 @@
 #pragma once
 
 int ps4KernelSymbolLookUp(const char *str, void **value);
-int static_lookup(const char *name, void **value, int swVer);
